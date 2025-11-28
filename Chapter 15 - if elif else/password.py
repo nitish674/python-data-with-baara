fbcae3375be67abe52b474276575c4a248157df4
nitish674 <nitishkumar11612@gmail.com>
2025-11-28 08:56:12 +0530

    Chapter-15 challenge 2(password.py) completed

--- Chapter 15 - if elif else/password.py
+++ Chapter 15 - if elif else/password.py
@@ -1,11 +1,27 @@
-password = ""
-valid = False
-if password:
+email = "test@gmail.com"
+password = input("Enter Your Password : ")
+valid = True
+if not password:
     print("Password must not be empty")
     valid = False
 if not len(password)>8:
     print("Password must be at least 8 characters")
-if not (password == password.lower()):
-    print("Password must contain at least 1 uppercase")
-if not (password == password.upper()):
-    print("Password must contain one lowercase")}
+    valid = False
+if not any(c.islower() for c in password):
+    print("Password must have at least 1 lowercase")
+    valid = False
+if not any(c.isupper() for c in password):
+    print("Password must have at least 1 Uppercase")
+    valid = False
+if not(password!=email):
+    print("Password must not be same as the email")
+    valid = False
+if not (password.count(" ")==0):
+    print("Password must not contain any spaces")
+    valid = False
+if not (password[0].isalnum() and password[-1].isalnum()):
+    print("password must start and end with a letter or digit")
+    valid = False
+if valid:
+    print("Password is valid")
+
